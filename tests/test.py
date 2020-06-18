f34668ce8ad14ca5c8877a0b48c0eeda834a1480
刘博 <564773807@qq.com>
2020-06-19 00:27:04 +0800

    发布为pip包

--- tests/test.py
+++ tests/test.py
@@ -4,10 +4,10 @@ Created on 2020/6/17 8:58 下午
 ---------
 @summary:
 ---------
-@author: liubo
+@author: Boirs
 """
 
 from jmpy.encrypt_py import start_encrypt
 
-input_file_path = "/Users/liubo/Downloads/test.py"
+input_file_path = "test.py"
 start_encrypt(input_file_path=input_file_path, output_file_path=None, ignore_files=None)
